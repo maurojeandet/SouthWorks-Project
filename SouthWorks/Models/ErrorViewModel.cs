9539684aa6f3a03d1ad48731869df65edb9bfd10
maurojeandet <40285566+maurojeandet@users.noreply.github.com>
2018-06-17 21:22:26 -0300

    southworksproject

---
+++ SouthWorks/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SouthWorks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
